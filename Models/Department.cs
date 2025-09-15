281391349d354400cfb4554aadf96230e5ab77d2
KareemWebber22 <kareem.a.khedr24@gmail.com>
2025-09-15 09:08:20 +0300

    Updating

---
+++ Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace ITI_MVC.Models
+{
+    public class Department
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public string ManagerName { get; set; }
+
+        public List<Employee>? Emps { get; set; }
+    }
+}
