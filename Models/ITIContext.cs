62a43f4f7bb02db6236daccfaed39e04fc83aeb8
KareemWebber22 <kareem.a.khedr24@gmail.com>
2025-09-15 09:18:24 +0300

    Create ITIContext.cs

---
+++ Models/ITIContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ITI_MVC.Models
+{
+    public class ITIContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        public ITIContext():base()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle
+                ("Data Source=met;User ID=KAREEM;Password=KAREEM");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
