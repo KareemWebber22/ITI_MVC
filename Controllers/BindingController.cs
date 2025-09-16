d610684de0eea440ffd9e17a07115aa7b97c3687
KareemWebber22 <kareem.a.khedr24@gmail.com>
2025-09-16 15:17:46 +0300

    Create BindingController.cs

---
+++ Controllers/BindingController.cs
@@ -0,0 +1,35 @@
+﻿using ITI_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_MVC.Controllers
+{
+    public class BindingController : Controller
+    {
+        //Request HTML Data
+        //Binding Primitive (int, string,....)
+        //Binding/TestPrimitive?name=ahmed&age=12&id=10&color=red&color=blue
+        //<form action= "Binding/TestPrimitive" method="get">
+        //    <input type="text" name="name">
+        //    <inout type="text" name="age">
+
+        public IActionResult TestPrimitive(string name, int age, int id, string[] color)
+        {
+            return Content($"{name} \t {age}");
+        }
+
+        //Binding Collection
+        //Binding/TestDict?Phones[Kareem]=123&Phones[Yahia]=456
+        public IActionResult TestDict(Dictionary<string, string> Phones, string name)
+        {
+            Phones["ahmed"] = "1234";
+            return Content("Ok");
+        }
+
+        //Binding/TestObj/3?DepartmentName=Automation&ManagerName=MostafaElghannam&color=red
+        public IActionResult TestObj(Department deptObj, string color)
+        {
+            return Content("Obj");
+        }
+    }
+}
+ 
